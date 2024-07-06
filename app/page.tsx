3e0528e158bdf905fe74a7d0034b24d30a9a93b4
PhilipBright <philipphoo123@gmail.com>
2024-07-06 13:09:30 +0700

    style: configuring the global css and tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import healthcare from '@/public/healthcare.webp'
+import React from "react";
 
 export default function Home() {
   return (
-   <h1>
-    Home
-   </h1>
+  <div className="flex h-screen max-h-screen">
+    <section className=" remove-scrollbar container my-auto ">
+      <div className=" sub-container max-w-[496px]">
+        <Image src={healthcare} width={1000} height={1000} alt="" className=" mb-12 h-10 w-fit" />
+      </div>
+    </section>
+  </div>
   );
 }
